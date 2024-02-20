86d887e755b40ac76ff380be1c81684925f8b1dd
jordanoskidavid <jordanoski.david16@gmail.com>
2024-02-20 01:04:09 +0100

    Making of register and login controllers

--- init/builder.sql
+++ init/builder.sql
@@ -2,10 +2,10 @@ create database Register;
 use Register;
 
 create table UserData(
+                         Email NVARCHAR(50) NOT NULL PRIMARY KEY,
                          Name NVARCHAR(30) NOT NULL,
                          Surname NVARCHAR(50) NOT NULL,
-                         Email NVARCHAR(50) NOT NULL PRIMARY KEY,
-                         Password NVARCHAR(50) NOT NULL,
+                         Password NVARCHAR(5000) NOT NULL,
                          PhoneNumber NVARCHAR(50) NOT NULL,
                          BirthDate DATE NOT NULL
 );
--- src/controllers/register.php
+++ src/controllers/register.php
@@ -9,41 +9,46 @@ $date_now = date("m/d/Y");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['pressed'] == 1) {
-        $user = $db->query1("SELECT Email from UserData WHERE Email='" . $_POST['email'] . "'");
+
+        $eMail = $_POST['email'];
+
+        $emailCheck = ($db->query1("SELECT Email FROM UserData WHERE Email='" . $eMail . "'"))->fetchAll();
+
+        foreach ($emailCheck as $userEmail) {
+            $tempUserEmail = htmlspecialchars($userEmail['Email']);
+        }
+        $emailCheck = $tempUserEmail;
+
         if (!Validator::string($_POST['name'], 3, 255)) {
             $errors['name'] = 'You have less than 3 or more than 255 characters, please enter valid input .';
         }
         if (!Validator::string($_POST['surname'], 3, 255)) {
             $errors['surname'] = 'You have less than 3 or more than 255 characters, please enter valid input .';
         }
-        if (!Validator::email($_POST['email'])) {
-            $errors['email'] = 'You have entered wrong type of characters for an email, please enter valid input .';
-        }
-        if (is_bool($user)) {
+        if (!Validator::email($emailCheck)) {
             $errors['email'] = 'User with this email already exist, please provide another email address or login.';
         }
         if (!Validator::password($_POST['psw'], 8, 255)) {
-            $errors['psw'] = 'You have to enter minimum 8 characters, 1 uppercase, 1 special character.';
+            $errors['psw'] = 'You have to enter minimum 8 characters';
         }
         if (!Validator::string($_POST['number'], 9, 9)) {
             $errors['number'] = 'You have less or more than 9 digits in your phone number.';
         }
-        if (empty($_POST['birthday'] ) ){
+        if (empty($_POST['birthday'])) {
             $errors['birthday'] = 'You have not entered your birth date';
         }
 
-        $hashedpsw = password_hash($_POST['psw'],PASSWORD_DEFAULT);
-
+        $hashedpsw = password_hash($_POST['psw'], PASSWORD_DEFAULT);
 
 
         if (empty($errors)) {
             $db->query("INSERT INTO UserData(Name,Surname,Email,Password,PhoneNumber,BirthDate) VALUES (:Name,:Surname,:Email,:Password,:PhoneNumber,:BirthDate)", [
                 'Name' => $_POST['name'],
-                'Surname'=>$_POST['surname'],
-                'Email'=>$_POST['email'],
-                'Password'=>$hashedpsw,
-                'PhoneNumber'=>$_POST['number'],
-                'BirthDate'=>$_POST['birthday'],
+                'Surname' => $_POST['surname'],
+                'Email' => $_POST['email'],
+                'Password' => $hashedpsw,
+                'PhoneNumber' => $_POST['number'],
+                'BirthDate' => $_POST['birthday'],
             ]);
         }
     }
